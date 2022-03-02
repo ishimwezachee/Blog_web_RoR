2fb87827f682151b5baec989f6663287a2eee79e
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-02 17:26:09 +0200

    fix routes path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "users/:user_id/posts" => "users#index"
-  get 'users/:user_id/posts/id' => "users#show"
-  get 'users/' => "posts#index"
-  get 'users/:id' =>"posts#show"
+  get 'users/:user_id/posts' => "posts#index"
+  get 'users/:user_id/posts/id' =>"posts#show"
+  get "users/" => "users#index"
+  get 'users/:id' => "users#show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
