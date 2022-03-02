b9dba953e10651c7339f70ea2abb4b4017f9987f
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-02 17:12:45 +0200

    create controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "users/:user_id/posts" => "users#index"
+  get 'users/:user_id/posts/id' => "users#show"
+  get 'users/' => "posts#index"
+  get 'users/:id' =>"posts#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
