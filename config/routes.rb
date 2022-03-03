cd9d174d408ee3ccde8adb8aca594025b740c573
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-03 10:19:00 +0200

    add linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'users/:user_id/posts' => "posts#index"
-  get 'users/:user_id/posts/id' =>"posts#show"
-  get "users/" => "users#index"
-  get 'users/:id' => "users#show"
+  get 'users/:user_id/posts' => 'posts#index'
+  get 'users/:user_id/posts/id' => 'posts#show'
+  get 'users/' => 'users#index'
+  get 'users/:id' => 'users#show'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
