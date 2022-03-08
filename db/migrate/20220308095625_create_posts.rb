fb7d06c26b288ca479743c5a07cc407ad72cbb96
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-08 16:26:22 +0200

    Add model and associations

--- db/migrate/20220308095625_create_posts.rb
+++ db/migrate/20220308095625_create_posts.rb
@@ -3,8 +3,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.integer :comment_counter
-      t.integer :likes_counter
 
       t.timestamps
     end
