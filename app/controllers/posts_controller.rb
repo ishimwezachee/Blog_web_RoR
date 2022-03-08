27f62c9bbc1cfbe72d2571cfce56ed4a641ef0ad
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-08 16:47:07 +0200

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This shiny device polishes bared foos
 class PostsController < ApplicationController
   def index; end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This shiny device polishes bared foos
 module ApplicationHelper
 end
