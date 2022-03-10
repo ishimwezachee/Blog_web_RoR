0182e45545f62b376b5fbda2ecf06a2f8a5148b8
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-10 12:15:15 +0200

    Number of posts and number of comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users, only: %i[index show] do
-  resources :posts, only: %i[index show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root to: 'users#index'
+  resources :users, only: %i[show index] do
+  resources :posts, only: %i[show index]
   end
 end
+
+
+# root 'users#index' resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] end 
