13386376024e0f146bf7626928befb7e6d92311e
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-11 13:36:40 +0200

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,32 @@
 class CommentsController < ApplicationController
-    def new
-      @comment = Comment.new
-    end
-  
-    def create
-      @post = Post.find(params[:post_id])
-      @new_comment = @post.comments.new(
-        text: comment_params,
-        author_id: @post.id,
-        post_id: @post.id
-      )
-      @new_comment.post_id = @post.id
-      @new_comment.update_comments_counter
-      respond_to do |format|
-        format.html do
-      if @new_comment.save
-        redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Success!'
-        # redirect_to user_post_path(@post.id, Post.find(params[:post_id]))
-      else
-        render :new, alert: 'Failed to Create!'
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @new_comment = @post.comments.new(
+      text: comment_params,
+      author_id: @post.id,
+      post_id: @post.id
+    )
+    @new_comment.post_id = @post.id
+    @new_comment.update_comments_counter
+    respond_to do |format|
+      format.html do
+        if @new_comment.save
+          redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Success!'
+          # redirect_to user_post_path(@post.id, Post.find(params[:post_id]))
+        else
+          render :new, alert: 'Failed to Create!'
+        end
       end
     end
-  end 
-end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)[:text]
-    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,25 +15,26 @@ class PostsController < ApplicationController
   end
 
   def create
-     # new object from params
-     @user = User.find(params[:user_id])
-     @new_post = @user.posts.new(post_parms)
-     @new_post.likes_counter = 0
-     @new_post.comment_counter = 0
-     respond_to do |format|
+    # new object from params
+    @user = User.find(params[:user_id])
+    @new_post = @user.posts.new(post_parms)
+    @new_post.likes_counter = 0
+    @new_post.comment_counter = 0
+    respond_to do |format|
       format.html do
         if @new_post.save
           flash.alert = 'Successful created'
           redirect_to "/users/#{@new_post.author.id}/posts/", notice: 'Created Successfully'
-          
+
         else
-          render :new, alert:'Failed to create'
+          render :new, alert: 'Failed to create'
         end
+      end
     end
   end
-end
 
- private
+  private
+
   def post_parms
     params.require(:post).permit(:title, :text)
   end
