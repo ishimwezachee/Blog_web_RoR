8e5203070b35090b2aee3d9d0c851aa5b041de06
ishimwezachee <zacheeishimwe@gmail.com>
2022-03-21 16:02:36 +0200

    fix log out routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :users, only: %i[index show] do 
     resources :posts, only: %i[index new create show]
